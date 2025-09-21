1198dcd5bf239c31f4057bd64a6c86113552a8f0
MatyaSina <matyassinaadugna@gmail.com>
2025-09-21 11:54:38 +0300

    updated information

--- first_app/urls.py
+++ first_app/urls.py
@@ -1,8 +1,21 @@
+# from django.conf.urls import url
+# from first_app import views
+
+# urlpatterns = [
+#     url(r'^$', views.index, name='index'),
+#     url(r'^users/',views.user, name='user'),
+#     url(r'^formpage/',views.form_name_view,name='form_name'),
+# ]
+
+
 from django.conf.urls import url
 from first_app import views
 
+# Template Tagging
+app_name = 'first_app'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^users/',views.user, name='user'),
-    url(r'^formpage/',views.form_name_view,name='form_name'),
+    url(r'^relative/$',views.relative,name='relative'),
+    url(r'^other/$',views.other,name='other')
 ]
+
--- first_app/views.py
+++ first_app/views.py
@@ -5,48 +5,61 @@ from . import forms
 from .forms import NewUserForm
 
 # Create your views here.
+# # Not Used
 # def index(request):
 #     my_dic = {"insert_me":"This is dynmic content coming from views"}
 #     print(my_dic["insert_me"])
 #     # text = "This is simple text from views.py"
 #     return render(request,"first_app/index.html",context=my_dic)
 
-def index(request):
-    my_web_access = AccessRecord.objects.all().order_by('date')
-    my_dic = {"access_record":my_web_access}
-    # print(my_dic["access_record"])
-    # text = "This is simple text from views.py"
-    return render(request,"first_app/index.html",context=my_dic)
+# def index(request):
+#     my_web_access = AccessRecord.objects.all().order_by('date')
+#     my_dic = {"access_record":my_web_access}
+#     # print(my_dic["access_record"])
+#     # text = "This is simple text from views.py"
+#     return render(request,"first_app/index.html",context=my_dic)
 
+# # Not Used
 # def user(request):
 #     user_acc = User.objects.all()
 #     user_info = {"user_info":user_acc}
 #     return render(request,"first_app/user.html",context=user_info)
 
-def user(request):
-    form = NewUserForm()
+# def user(request):
+#     form = NewUserForm()
 
-    if request.method == 'POST':
-        form = NewUserForm(request.POST)
+#     if request.method == 'POST':
+#         form = NewUserForm(request.POST)
 
-        if form.is_valid():
-            form.save(commit=True)
-            return index(request)
-        else:
-            print('Form is Invalid')
-    return render(request,"first_app/user.html",{'form':form})
+#         if form.is_valid():
+#             form.save(commit=True)
+#             return index(request)
+#         else:
+#             print('Form is Invalid')
+#     return render(request,"first_app/user.html",{'form':form})
 
-def form_name_view(request):
-    form = forms.FormName()
+# def form_name_view(request):
+#     form = forms.FormName()
 
-    if request.method == "POST":
-        form = forms.FormName(request.POST)
+#     if request.method == "POST":
+#         form = forms.FormName(request.POST)
 
-        if form.is_valid():
-            # DO SOMETHING CODE
-            print("VALIDATION SUCCESS!")
-            print("NAME: "+form.cleaned_data['name'])
-            print("EMAIL: "+form.cleaned_data['email'])
-            print("TEX: "+form.cleaned_data['text'])
+#         if form.is_valid():
+#             # DO SOMETHING CODE
+#             print("VALIDATION SUCCESS!")
+#             print("NAME: "+form.cleaned_data['name'])
+#             print("EMAIL: "+form.cleaned_data['email'])
+#             print("TEX: "+form.cleaned_data['text'])
 
-    return render(request,'first_app/form_page.html',{'form':form})
+#     return render(request,'first_app/form_page.html',{'form':form})
+
+
+def index(request):
+    context_dict = {'text':'hello world','number':100}
+    return render(request,'first_app/index.html',context_dict)
+
+def other(request):
+    return render(request,'first_app/other.html')
+
+def relative(request):
+    return render(request,'first_app/relative_url_template.html')
